412acad988734ad1856550d7026ba7e40e507308
Charles Ryu <charlesworld1994@gmail.com>
2022-01-23 13:58:03 -0800

    workin on quiz

---
+++ quize_app/script.js
@@ -0,0 +1,78 @@
+const quizData = [{
+        question: "When did America was Found?",
+        a: "1768",
+        b: "1675",
+        c: "1645",
+        d: "1776",
+        correct: "d",
+    },
+    {
+        question: "What is the best programming language?",
+        a: "Django",
+        b: "React",
+        c: "Ruby On Rails",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "Who found America?",
+        a: "Jonny Debs",
+        b: "Tom Hanks",
+        c: "Jimmy Falon",
+        d: "None of above",
+        correct: "d",
+    },
+    {
+        question: "Who is the best actor?",
+        a: "Tom Hanks",
+        b: "Kim Collins",
+        c: "Maddy Collins",
+        d: "Charles Ryu",
+        correct: "c",
+    },
+    {
+        question: "Why you gay?",
+        a: "who says I'm Gay?",
+        b: "You are Gay",
+        c: "I'm Not GAY.",
+        d: "Yes, You. Are.",
+        correct: "d",
+    },
+    {
+        question: "Who Is US president?",
+        a: "Joe",
+        b: "Donald",
+        c: "Charles",
+        d: "Maddy",
+        correct: "c",
+    },
+];
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+submitBtn.addEventListener("click", () => {
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
+        // TODO: show results
+        alert("You finished!");
+    }
+});
